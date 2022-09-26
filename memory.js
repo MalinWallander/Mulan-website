9f979bfb129c60ae43e0ef26ce55ae59e5c4ea5b
MalinW <maalinwallander@gmail.com>
2022-09-26 11:40:45 +0200

    memory done

---
+++ memory.js
@@ -0,0 +1,136 @@
+function card1Clicked(){
+    let card1=document.getElementById('card1');
+    card1.classList.toggle('card-1-flipped');
+}
+
+let card1=document.getElementById('card1');
+card1.addEventListener('click', card1Clicked);
+
+
+function card2Clicked(){
+    let card2=document.getElementById('card2');
+    card2.classList.toggle('card-2-flipped');
+}
+
+let card2=document.getElementById('card2');
+card2.addEventListener('click', card2Clicked);
+
+
+function card3clicked(){
+    let card3=document.getElementById('card3');
+    card3.classList.toggle('card-3-flipped');
+}
+
+let card3=document.getElementById('card3');
+card3.addEventListener('click', card3clicked);
+
+
+function card4clicked(){ 
+    let card4=document.getElementById('card4');
+card4.classList.toggle('card-4-flipped'); 
+} 
+
+let card4=document.getElementById('card4'); 
+card4.addEventListener('click', card4clicked);
+
+
+function card5clicked()
+{ let card5=document.getElementById('card5');
+card5.classList.toggle('card-5-flipped'); 
+} 
+let card5=document.getElementById('card5'); 
+card5.addEventListener('click',card5clicked);
+
+
+function card6clicked()
+{ let card6=document.getElementById('card6');
+card6.classList.toggle('card-6-flipped'); 
+} 
+let card6=document.getElementById('card6'); 
+card6.addEventListener('click',card6clicked);
+
+
+function card7clicked()
+{ let card7=document.getElementById('card7');
+card7.classList.toggle('card-7-flipped'); 
+} 
+let card7=document.getElementById('card7'); 
+card7.addEventListener('click',card7clicked);
+
+
+function card8clicked()
+{ let card8=document.getElementById('card8');
+card8.classList.toggle('card-8-flipped'); 
+} 
+let card8=document.getElementById('card8'); 
+card8.addEventListener('click',card8clicked);
+
+
+function card9clicked()
+{ let card9=document.getElementById('card9');
+card9.classList.toggle('card-9-flipped'); 
+} 
+
+let card9=document.getElementById('card9'); 
+card9.addEventListener('click',card9clicked);
+
+
+function card10clicked()
+{ let card10=document.getElementById('card10');
+card10.classList.toggle('card-10-flipped'); 
+} 
+
+let card10=document.getElementById('card10'); 
+card10.addEventListener('click',card10clicked);
+
+function card11clicked()
+{ let card11=document.getElementById('card11');
+card11.classList.toggle('card-11-flipped'); 
+} 
+let card11=document.getElementById('card11'); 
+card11.addEventListener('click',card11clicked);
+
+
+function card12clicked()
+{ let card12=document.getElementById('card12');
+card12.classList.toggle('card-12-flipped'); 
+} 
+
+let card12=document.getElementById('card12'); 
+card12.addEventListener('click',card12clicked);
+
+
+function card13clicked()
+{ let card13=document.getElementById('card13');
+card13.classList.toggle('card-13-flipped'); 
+} 
+
+let card13=document.getElementById('card13'); 
+card13.addEventListener('click',card13clicked);
+
+
+function card14clicked()
+{ let card14=document.getElementById('card14');
+card14.classList.toggle('card-14-flipped'); 
+} 
+
+let card14=document.getElementById('card14'); 
+card14.addEventListener('click',card14clicked);
+
+
+function card15clicked()
+{ let card15=document.getElementById('card15');
+card15.classList.toggle('card-15-flipped'); 
+} 
+
+let card15=document.getElementById('card15'); 
+card15.addEventListener('click',card15clicked);
+
+
+function card16clicked()
+{ let card16=document.getElementById('card16');
+card16.classList.toggle('card-16-flipped'); 
+} 
+
+let card16=document.getElementById('card16'); 
+card16.addEventListener('click',card16clicked);
