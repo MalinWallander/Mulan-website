c37cb2f00d811ba4514ea2db42f11649b15dc6ab
MalinW <maalinwallander@gmail.com>
2022-11-14 14:01:53 +0100

    final?

---
+++ Website/script.js
@@ -0,0 +1,12 @@
+let name = 'Malin';
+let a = '10';
+let b = '15';
+
+let greeting = 'Hello,';
+
+alert(greeting + ' ' + name);
+
+document.getElementById('intro').innerHTML='Hello Malin';
+document.getElementById('production').style.color='green';
+document.getElementById('production').style.backgroundColor='red';
+
